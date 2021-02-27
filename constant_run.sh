42fb332dd2edce6ceb467ad2157a8c4ce4996d7a
Zain Shafique <zshafiqu@ucsc.edu>
2021-02-26 19:16:28 -0800

    hot fix

--- constant_run.sh
+++ constant_run.sh
@@ -4,14 +4,25 @@
 COMMAND='python3 app_driver.py'
 LOGFILE=./misc/shell-script-logs.txt
 
+# If the script fails, move the python script logs to an old folder and denote them by the date the script failed
+MOVECOMMAND = 'mv ./misc/python-script-logs.txt ./misc/old_logs/python-script-logs-ending-${}.txt'
+
 writelog() {
   now=`date`
   echo "$now $*" >> $LOGFILE
 }
 
-writelog "Starting"
+moveoldscript() {
+    # If the script fails, move the python script logs to an old folder and denote them by the date the script failed
+    curr=`date`
+    MOVECOMMAND = 'mv ./misc/python-script-logs.txt ./misc/old_logs/python-script-logs-ending-${curr}.txt'
+    $MOVECOMMAND
+}
+
+writelog "Shell script Starting"
 while true ; do
   $COMMAND
   writelog "Exited python script with status $?"
   writelog "Restarting python script"
+  moveoldscript
 done
